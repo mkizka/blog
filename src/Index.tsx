a8dc44cc16b84ac3b00541bc7efacacde72403cb
mkizka <g.mkizka@gmail.com>
2022-01-06 16:17:57 +0900

    記事を日付順に

--- src/Index.tsx
+++ src/Index.tsx
@@ -4,11 +4,21 @@ import { entries } from "./utils/entries";
 import { md2frontmatter } from "./utils/transformers";
 
 function Index() {
+  const pages = Object.entries(entries)
+    .map(([entryId, entry]) => {
+      return {
+        path: `/entry/${entryId}`,
+        meta: md2frontmatter(entry),
+      };
+    })
+    .sort((a, b) => {
+      return +new Date(b.meta.date) - +new Date(a.meta.date);
+    });
   return (
     <ul>
-      {Object.entries(entries).map(([entryId, entry]) => (
-        <li key={entryId}>
-          <Link to={`/entry/${entryId}`}>{md2frontmatter(entry).title}</Link>
+      {pages.map((page) => (
+        <li key={page.path}>
+          <Link to={page.path}>{page.meta.title}</Link>
         </li>
       ))}
     </ul>
