ae44c0ee4a83b20e3ab2cf31ec5c02ee7cefae7d
mkizka <g.mkizka@gmail.com>
2022-08-03 21:41:48 +0900

    css移動

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import "dayjs/locale/ja";
+import "./index.css";
 
 dayjs.locale("ja");
 dayjs.extend(relativeTime);
