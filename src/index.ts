ed7a03b32fd1d4490ab0bdcc75a572e5ae93d8de
mkizka <g.mkizka@gmail.com>
2026-05-28 22:22:59 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { badge } from "./funcs/badge";
 import { image } from "./funcs/image";
 import { noindex } from "./funcs/noindex";
+
+// @ts-expect-error
 import "./index.css";
 
 window.addEventListener("load", () => {
